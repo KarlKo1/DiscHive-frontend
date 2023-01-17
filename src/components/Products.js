4939626b1a95ad2968f8b5011149abe06e73cd4c
KarlKo1 <kongopoiss@gmail.com>
2023-01-17 23:11:58 +0200

    Added linking between pages

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,13 +1,16 @@
 import { ProductStyle } from "../styles/ProductStyle";
+import Link from "next/link";
 
 export default function Products({ product }) {
   //Extract the info from props
-  const { title, price, image } = product.attributes;
+  const { title, price, image, slug } = product.attributes;
   return (
     <ProductStyle>
-      <div>
-        <img src={image.data[0].attributes.formats.small.url} alt="" />
-      </div>
+      <Link href={`/product/${slug}`}>
+        <div>
+          <img src={image.data[0].attributes.formats.small.url} alt="" />
+        </div>
+      </Link>
       <h2>{title}</h2>
       <h2>{price}</h2>
     </ProductStyle>
