a46580890fdabfbeea563b29cf0ee9ae8499335a
KarlKo1 <kongopoiss@gmail.com>
2023-01-18 13:08:55 +0200

    Added cart component, styling for it and remove/add product

---
+++ src/components/Cart.js
@@ -0,0 +1,52 @@
+import { useStateContext } from "../lib/context";
+import {
+  CartWrapper,
+  CartStyle,
+  Card,
+  CardInfo,
+  EmptyStyle,
+} from "../styles/CartStyle";
+import { FiShoppingCart } from "react-icons/fi";
+import { BsFillPlusCircleFill, BsFillDashCircleFill } from "react-icons/bs";
+import { Quantity } from "../styles/ProductDetails";
+
+export default function Cart() {
+  const { cartItems, setShowCart, onAdd, onRemove } = useStateContext();
+  return (
+    <CartWrapper onClick={() => setShowCart(false)}>
+      <CartStyle onClick={(e) => e.stopPropagation()}>
+        {cartItems.length < 1 && (
+          <EmptyStyle>
+            <h1>Your shopping cart is empty!</h1>
+            <FiShoppingCart />
+          </EmptyStyle>
+        )}
+        {cartItems.length >= 1 &&
+          cartItems.map((item) => {
+            return (
+              <Card key={item.slug}>
+                <img
+                  src={item.image.data[0].attributes.formats.thumbnail.url}
+                  alt={item.title}
+                />
+                <CardInfo>
+                  <h3>{item.title}</h3>
+                  <h3>{item.price}</h3>
+                  <Quantity>
+                    <span>Quantity</span>
+                    <button onClick={() => onRemove(item)}>
+                      <BsFillDashCircleFill />
+                    </button>
+                    <span>{item.quantity}</span>{" "}
+                    <button onClick={() => onAdd(item, 1)}>
+                      <BsFillPlusCircleFill />
+                    </button>
+                  </Quantity>
+                </CardInfo>
+              </Card>
+            );
+          })}
+      </CartStyle>
+    </CartWrapper>
+  );
+}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,21 @@
 import Link from "next/link";
 import { FiShoppingCart } from "react-icons/fi";
 import { NavStyle, NavItems } from "../styles/NavStyle";
+import Cart from "./Cart";
+import { useStateContext } from "../lib/context";
 
 export default function Nav() {
+  const { showCart, setShowCart } = useStateContext();
   return (
     <NavStyle>
       <Link href={"/"}>DiscWorld</Link>
       <NavItems>
-        <div>
+        <div onClick={() => setShowCart(true)}>
           <FiShoppingCart />
           <h3>Cart</h3>
         </div>
       </NavItems>
+      {showCart && <Cart />}
     </NavStyle>
   );
 }
