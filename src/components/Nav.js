f6a5974f6fb32498b04bd84d6b55c7a250d96828
KarlKo1 <kongopoiss@gmail.com>
2023-01-20 17:18:23 +0200

    Added auth0 login

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,13 +5,17 @@ import Cart from "./Cart";
 import { useStateContext } from "../lib/context";
 import { AnimatePresence } from "framer-motion";
 import { motion } from "framer-motion";
+import User from "./User";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Nav() {
   const { showCart, setShowCart, totalQuantity } = useStateContext();
+  const { user, error, isLoading } = useUser();
   return (
     <NavStyle>
       <Link href={"/"}>DiscWorld</Link>
       <NavItems>
+        <User />
         <div onClick={() => setShowCart(true)}>
           {totalQuantity > 0 && (
             <motion.span initial={{ scale: 0 }} animate={{ scale: 1 }}>
